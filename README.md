3a5b3c3c07783b818bb8d53debd264e6796c2d8b
dymaic-entropy-enterprise <dynen.enterprise@gmail.com>
2023-12-22 17:00:14 +0100

    add documentation and license

--- README.md
+++ README.md
@@ -4,16 +4,27 @@ disegna cazzi su command line
 # Motivazione
 te gusta er pipo
 
+# Installazione
+cazzo.py dipende dai seguenti programmi
+ - `clear` (per sistemi unix)
+ - `cls` (per sistemi windows)
+ 
+dovrebbero essere presintallati 
+
+dopo essersi assicurati che i programmi siano presenti clonare la repo
+
 # Utilizzo
 `cazzo.py` può disegnare cazzi di lunghezza fissa o variabile a seconda del numero di  argomenti passati in command line
 
 ## Lunghezza fissa
+nella directory della repo, girare
 ```bash
 python cazzo.py <length>
 ```
 disegna un cazzo di lunghezza `<length>`
 
 ## Lunghezza variabile
+nella directory della repo, girare
 ```bash
 python cazzo.py <min-length> <max-length>
 ```
@@ -21,3 +32,9 @@ diesgna un cazzo la cui lunghezza varia tra `<min-length>` e `<max-length>` nel 
 ```bash
 python cazzo.py <min-length> <max-length> <time(millis)>
 ```
+
+# TODO
+ - parametrizzare la larghezza del cazzo
+ - permettere tipi diversi di cazzo (es. circonciso, non circonciso)
+ - file di config per il cazzo
+ - opzione per far partire "ho litigato con mia moglie" di Gianni Celeste ogni volta che giri lo script
--- cazzo.py
+++ cazzo.py
@@ -24,7 +24,29 @@ cazzo_top:str = """   +--           --+
     +------|------+
 """
 
+def sleep_millis(millis:int):
+    """
+    sleep for time specified in milliseconds
+
+    @param millis: the milliseconds to sleep for
+    """
+
+    sleep(millis/1000)
+
+def clear_terminal():
+    """
+    clears terminal
+    """
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def cazzo_draw(length:int):
+    """
+    disenga un cazzo lungo `length` sul terminale
+
+    @param length: la lunghezza del cazzo
+    """
+
     print(cazzo_base,end="")
     for _ in range(length):
         print(cazzo_extender)
@@ -32,20 +54,28 @@ def cazzo_draw(length:int):
 
 
 def cazzo_loop(min_len:int, max_len:int, millis:int):
+    """
+    disenga un cazzo lungo `length` sul terminale
+
+    @param min_len: la lunghezza del cazzo
+    @param max_len: la lunghezza del cazzo
+    @param millis: periodio di transizione tra lunghezze, in millisecondi
+    """
     while True:
+        def cazzo_loop_iteration(l:int):
+            cazzo_draw(l)
+            sleep_millis(millis)
+            clear_terminal()
+            
         for i in range(min_len, max_len):
-            cazzo_draw(i)
-            sleep(millis/1000)
-            os.system('clear')
+            cazzo_loop_iteration(i)
 
         for i in range(max_len -1 , min_len + 1, -1):
-            cazzo_draw(i)
-            sleep(millis/1000)
-            os.system('clear')
+            cazzo_loop_iteration(i)
 
 if __name__=="__main__":
     if len(argv) < 2:
-        print("vaffanculo")
+        print("cazzo.py richiede almeno un parametro(intero) da command line")
         exit(1)
     if len(argv) == 2:
         cazzo_draw(int(argv[1]))
@@ -54,4 +84,7 @@ if __name__=="__main__":
         cazzo_loop(int(argv[1]), int(argv[2]), 50)
     if len(argv) == 4:
         cazzo_loop(int(argv[1]), int(argv[2]), int(argv[3]))
+    if len(argv) > 4:
+        print("cazzo.py richiede meno di 4 parametri(interi), 4 o più non vale")
+        exit(1)
 
